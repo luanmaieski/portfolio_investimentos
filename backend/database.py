691235949affb39ecb75b6f2505f2f085a11943f
Luan Maieski <luanmaieski@gmail.com>
2025-09-03 14:21:48 -0300

    criando arquivo de conexão com o banco de dados

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from config import settings
 
-POSTGRES_DATABASE_URL = "postgresql://user:password@postgres/mydatabase"
+#POSTGRES_DATABASE_URL = "postgresql://user:password@postgres/mydatabase"
+#DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/portfolio"
+engine = create_engine(settings.DATABASE_URL)
+#engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 
-engine = create_engine(POSTGRES_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base() #ORM
 
